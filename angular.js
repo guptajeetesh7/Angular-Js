2d51f05bf7abc5a22f0fccbdbf3261c599cabcba
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-05-08 22:11:05 +0530

    first commit

--- angular.js
+++ angular.js
@@ -1,12 +1,43 @@
 
 
-
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+app.config(function($routeProvider ) {
     $routeProvider
     
     .when('/', {
-        template : "asdasd"
-    });
+        templateUrl : "login.html"
+    })
+    .when('/dashboard', {
+    	
+    	resolve : {
+
+    		'check' : function($rootScope ,$location){
+    			if (!$rootScope.loggedIn) {
+    				$location.path("/");
+    			}
+    		}
+
+    	},
+
+        templateUrl : "dashboard.html"
+
+            })    ;
     
+});
+
+
+app.controller('loginCtrl',function($scope , $location ,$rootScope){
+
+	 $scope.submit=function(){
+
+	if($scope.username== "admin" && $scope.password=="admin")
+	{
+	     $rootScope.loggedIn=true;	
+		$location.path('/dashboard');
+	}
+	else{
+		alert("Wrong account");
+	}
+  }
+
 });
