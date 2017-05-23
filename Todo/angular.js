423e3944f1ffa2f7a3bace551fbc53f077f889bf
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-05-23 23:55:18 +0530

    first commit

--- Todo/angular.js
+++ Todo/angular.js
@@ -4,6 +4,13 @@ app.controller ("todo",function($scope){
 
 	$scope.Tasks =[];
 
+	var taskData = localStorage['tasksList'];
+
+	if(taskData !== undefined)
+	{
+		$scope.Tasks= JSON.parse(taskData);
+	}
+
 	$scope.submit = function(){
 
 		if(event.which==13 && $scope.task!="")
@@ -16,20 +23,27 @@ app.controller ("todo",function($scope){
 	$scope.addTask =function(){
   
 		$scope.Tasks.push({"taskMessage" : $scope.task , "status" : false});
-		console.log($scope.Tasks);
-		
+		localStorage['tasksList'] = JSON.stringify($scope.Tasks);
+		console.log(localStorage);
 	};
 
-	$scope.contentedit = function(){
+	$scope.contentedit = function(msg){
 
 		event.target.contentEditable = event.target.contentEditable=="false" ? "true" : "false" ;
-		console.log("sadoasdk");
+		for( i=0 ; i< $scope.Tasks.length ; i++)
+		{
+			if($scope.Tasks[i].taskMessage==msg){
+				$scope.Tasks[i].taskMessage= event.target.innerText;
+			}
+		}
+		localStorage['tasksList'] = JSON.stringify($scope.Tasks);
 	};
 
 	$scope.enterAgain = function(msg){
 
 		if(event.which==13 && msg!="")
-			$scope.contentedit();
+			$scope.contentedit(msg);
 	};
 
+	
 });
