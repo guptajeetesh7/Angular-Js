3a9303116875d57b08fa16fa063c7dd28bc70b28
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-05-24 21:48:39 +0530

    Front-end added

--- weather/angular.js
+++ weather/angular.js
@@ -25,15 +25,26 @@ classApp.controller('weatherCtrl',function($scope ,$http){
 
 		console.log("http://ip-api.com/json");
 
-		var openWeatherUrl ="api.openweathermap.org/data/2.5/weather?lat="+ $scope.lat + "&lon="+ $scope.lon +"&appid=" + apiKey  ;
+		var openWeatherUrl ="http://api.openweathermap.org/data/2.5/weather?lat="+ $scope.lat + "&lon="+ $scope.lon +"&appid=c242d2031036ff05b3bd8559f91e2449" ;
+
+		
+		
 
 		$http.get(openWeatherUrl).success(function(data){
 
-			$scope.description = data.weather[0].description;
-			$scope.speed = (2.237*data.wind.speed).toFixed(1) + "mph";
-			$scope.name = data.name;
+			console.log(openWeatherUrl);
+			console.log("Connection done");
+
+		$scope.description = data.weather[0].description;
+		$scope.speed = (2.237*data.wind.speed).toFixed(1) + "mph";
+		$scope.name = data.name;
 			$scope.temp = data.main.temp;
-			
+			$scope.fTemp = ($scope.temp *(9/5)-459).toFixed(1) + " F";
+			$scope.cTemp = ($scope.temp-273).toFixed(1) + "C" ;
+
+			$scope.icon = "http://openweathermap.org/img/w/" + data.weather[0].icon +".png";
+
+
 
 			
 		});
